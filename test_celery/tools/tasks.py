c1d33c120a754f0e4e71ea9c476295c6d78985ef
yanshigou <569578851@qq.com>
2018-12-27 21:41:27 +0800

    跑流量task 查询task

--- test_celery/tools/tasks.py
+++ test_celery/tools/tasks.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from celery import shared_task
-import time
+import json
 import xlrd
 import xlwt
 from datetime import datetime
@@ -9,7 +9,9 @@ from paopao.models import PLL
 import requests
 
 
-SERVER_URL = "此URL上传文件在某个接口访问/查询/命令等的链接"
+SERVER_URL = "http://ip地址/commander/paopao/"
+GET_SERVER_URL = "http://ip地址/common/querydownload/?imei="
+# SERVER_URL = "http://ip地址/smartlock/checkimei/"
 
 
 @shared_task(track_started=True)
@@ -25,7 +27,7 @@ def test_task(time1):
 
     cols = sheet1.col_values(0)  # 获取列内容
     ok_imeis = []
-    error_imeis = []
+    offline_imeis = []
 
     for col in cols:
         # print col
@@ -36,10 +38,8 @@ def test_task(time1):
         imei = str(int(col))
         # print imei
         if len(imei) != 15:
-            error_imeis.append(imei)
             continue
         else:
-            ok_imeis.append(imei)
             data = {"imei": imei}
             try:
                 res = requests.post(SERVER_URL, data=data)
@@ -47,8 +47,10 @@ def test_task(time1):
                 error_no = res.json()["error_no"]
                 if error_no == 0:
                     print("pao imei=" + imei)
+                    ok_imeis.append(imei)
                 elif error_no == 4:
                     print("offline imei=" + imei)
+                    offline_imeis.append(imei)
                 else:
                     print("error imei=" + imei)
             except:
@@ -60,8 +62,8 @@ def test_task(time1):
         sheet1.write(i, 0, str(ok_imeis[i]))
 
     sheet2 = f.add_sheet('invalidIMEI', cell_overwrite_ok=True)
-    for i in range(0, len(error_imeis)):
-        sheet2.write(i, 0, error_imeis[i])
+    for i in range(0, len(offline_imeis)):
+        sheet2.write(i, 0, offline_imeis[i])
 
     print(filename)
     print(type(filename))
@@ -77,7 +79,81 @@ def test_task(time1):
         new_imei_file = 'upload/download/paoliuliang_imei.xls'
 
     f.save(new_imei_file)
-    count = len(ok_imeis) + len(error_imeis)
+    count = len(ok_imeis) + len(offline_imeis)
+
+    p = PLL.objects.get(starttime=time1)
+    p.oktime = datetime.now()
+    p.download = new_imei_file
+    p.count = count
+    p.save()
+    return
+
+
+@shared_task(track_started=True)
+def get_imei(time1):
+    filename = PLL.objects.get(starttime=time1).filename
+    print(filename)
+
+    src = 'upload/%s' % filename  # 连接成上传文件的路径
+    print(src)
+    rb = xlrd.open_workbook(filename=src)  # 打开文件
+    sheet1 = rb.sheet_by_index(0)  # 通过索引获取表格
+    print(sheet1.name)
+
+    cols = sheet1.col_values(0)  # 获取列内容
+    ok_imeis = []
+    no_imeis = []
+
+    for col in cols:
+        # print col
+        # if len(col)< 14:
+        #     continue
+        if col == 'imei':
+            continue
+        imei = str(int(col))
+        # print imei
+        if len(imei) != 15:
+            continue
+        else:
+            try:
+                res = requests.get(url=GET_SERVER_URL+imei).content
+                res = json.loads(res)
+                print(res)
+                exist = res.get('exist')
+                if exist is False:
+                    no_imeis.append(imei)
+                    print(imei + "false")
+                elif exist is True:
+                    ok_imeis.append(imei)
+                    print(imei + "true")
+            except:
+                print("error imei=" + imei)
+
+    f = xlwt.Workbook()
+    sheet1 = f.add_sheet('OK_IMEI', cell_overwrite_ok=True)
+    for i in range(0, len(ok_imeis)):
+        sheet1.write(i, 0, str(ok_imeis[i]))
+        sheet1.write(i, 1, "OK")
+    sheet2 = f.add_sheet('No_IMEI', cell_overwrite_ok=True)
+    for i in range(0, len(no_imeis)):
+        sheet2.write(i, 0, no_imeis[i])
+        sheet2.write(i, 1, "No")
+
+    print(filename)
+    print(type(filename))
+
+    filename = str(filename).split('/')[1]
+    print(filename)
+
+    if filename[-3:] == 'xls':
+        new_imei_file = 'upload/download/result_' + filename
+    elif filename[-4:] == 'xlsx':
+        new_imei_file = 'upload/download/result_' + filename[:-1]
+    else:
+        new_imei_file = 'upload/download/result_imei.xls'
+
+    f.save(new_imei_file)
+    count = len(ok_imeis) + len(no_imeis)
 
     p = PLL.objects.get(starttime=time1)
     p.oktime = datetime.now()
