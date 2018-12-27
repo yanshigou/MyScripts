c458abf8531e4dfd39ad719e14405e8af463a092
yanshigou <569578851@qq.com>
2018-12-27 21:26:46 +0800

    uwsgi restart

---
+++ test_celery/restart.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+NAME="walkerfree"
+if [ ! -n "$NAME" ];then
+    echo "no arguments"
+    exit;
+fi
+
+echo $NAME
+ID=`ps -ef | grep "$NAME" | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
+echo $ID
+echo "################################################"
+for id in $ID
+do
+kill -9 $id
+echo "kill $id"
+done
+echo  "################################################"
+nohup uwsgi --ini test_celery_uwsgi.ini &
