42c87227ce768f538857a3ef5a64c22470e2d5ce
yanshigou <569578851@qq.com>
2018-12-27 21:34:15 +0800

    登录forms

--- test_celery/paopao/forms.py
+++ test_celery/paopao/forms.py
@@ -6,5 +6,10 @@ from django import forms
 
 
 class ImeiInfoForm(forms.Form):
-    info = forms.CharField()
-    filename = forms.FileField()
+    info = forms.CharField(label=u'文件备注', min_length=1)
+    filename = forms.FileField(label=u'上传文件')
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, min_length=5)
