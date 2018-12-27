619f92e3b7a7e7b149779ede3014aae895ee19fb
yanshigou <569578851@qq.com>
2018-12-27 21:34:51 +0800

    增加用户登录功能

--- test_celery/paopao/models.py
+++ test_celery/paopao/models.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class UserProfile(AbstractUser):
+    nick_name = models.CharField(max_length=50, verbose_name=u'昵称', default='')
+    company = models.CharField(max_length=100, default='', verbose_name=u'公司名称')
+    mobile = models.CharField(max_length=11, null=True, blank=True)
+
+    class Meta:
+        verbose_name = '用户信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
 
 
 class PLL(models.Model):
@@ -11,4 +25,6 @@ class PLL(models.Model):
     starttime = models.DateTimeField(null=True, blank=True, verbose_name=u'开始时间')
     oktime = models.DateTimeField(null=True, blank=True, verbose_name=u'完成时间')
     download = models.FileField(upload_to='download', verbose_name=u'资源文件', max_length=100)
+
+
 
